e4ffc44b1cbaf33c3331452ae303d8cdf3c9ca94
copperfox <copperfox@mail.ru>
2020-05-28 14:32:54 +0300

    slightly updatet according to task

--- src/components/validation-helper.js
+++ src/components/validation-helper.js
@@ -12,16 +12,16 @@ export function ValidationHelper(props) {
   return (
     <div>
       <div>
-        {!e.street1  ? f.street1 : <Nodata/> }{' '} 
-        {!e.city1 ? f.city1 : <Nodata/> }{' '} 
-        {!e.stateCode1 ? f.stateCode1 : <Nodata/> }{' '}
-        {!e.postalCode1 ? f.postalCode1 : <Nodata/> }{' '}
+        {!e.street1 && f.street1.length>0   ? f.street1 : <Nodata/> }{' '} 
+        {!e.city1 && f.city1.length>0 ? f.city1 : <Nodata/> }{' '} 
+        {!e.stateCode1 && f.stateCode1.length>0 ? f.stateCode1 : <Nodata/> }{' '}
+        {!e.postalCode1 && f.postalCode1.length>0 ? f.postalCode1 : <Nodata/> }{' '}
       </div>
       <div>
-        {!e.street2 ? f.street2 : <Nodata/> }{' '} 
-        {!e.city2 ? f.city2 : <Nodata/> }{' '} 
-        {!e.stateCode2 ? f.stateCode2 : <Nodata/> }{' '}
-        {!e.postalCode2 ? f.postalCode2 : <Nodata/> }{' '}
+        {!e.street2 && f.street2.length>0   ? f.street2 : <Nodata/> }{' '} 
+        {!e.city2 && f.city2.length>0 ? f.city2 : <Nodata/> }{' '} 
+        {!e.stateCode2 && f.stateCode2.length>0 ? f.stateCode2 : <Nodata/> }{' '}
+        {!e.postalCode2 && f.postalCode2.length>0 ? f.postalCode2 : <Nodata/> }{' '}
       </div>
       <br/>
     </div>
